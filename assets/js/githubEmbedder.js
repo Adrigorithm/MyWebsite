b332b81823ba3f83d842fe3f55369ab9f3636c5c
Adrigorithm <2ndalgorithm@gmail.com>
2024-03-28 23:37:27 +0100

    github basic embed JS done

--- assets/js/githubEmbedder.js
+++ assets/js/githubEmbedder.js
@@ -1,6 +1,44 @@
+import { WriteMode } from "./enums";
+import { DateStringToDate, SetTextContent } from "./utilities";
+
 class GithubEmbedder{
     static fromPublicRepoJSON(json){
+        let container = document.createElement("div");
+        let summary = document.createElement("div");
+        let extended = document.createElement("div");
+        let titleLink = document.createElement("a");
+        let title = document.createElement("h3");
+        let ownerLink = document.createElement("a");
+        let owner = document.createElement("h4");
+        let ownerPrefix = document.createElement("span");
+        let description = document.createElement("p");
+        let updated = document.createElement("p");
 
+        let titleText = document.createTextNode(json.name);
+        let ownerText = document.createTextNode(json.owner.login);
+        let ownerPrefixText = document.createTextNode("created by ");
+        let descriptionText = document.createTextNode(json.description);
+        let updatedText = document.createTextNode(`Last updated at ${DateStringToDate(json.pushed_at)}`);
+
+        titleLink.setAttribute("href", json.html_url);
+        titleLink.setAttribute("target", "_blank");
+        ownerLink.setAttribute("href", json.owner.html_url);
+        ownerLink.setAttribute("target", "_blank");
+
+        SetTextContent(description, WriteMode.SET, descriptionText);
+        SetTextContent(updated, WriteMode.SET, updatedText);
+        SetTextContent(title, WriteMode.SET, titleText);
+        SetTextContent(ownerPrefix, WriteMode.SET, ownerPrefixText);
+        owner.appendChild(ownerPrefix);
+        SetTextContent(owner, WriteMode.APPEND, ownerText);
+
+        titleLink.appendChild(title);
+        ownerLink.appendChild(owner);
+        summary.append(titleLink, ownerLink);
+        extended.append(description, updated);
+        container.append(summary, extended);
+
+        return container;
     }
 }
 
--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -2,16 +2,23 @@
 
 import { NotImplementedException } from "./exceptions.js";
 
-function SetTextContent(nodeToWriteTo, writeMode, ... contentNodes) {
+function SetTextContent(nodeToWriteTo, writeMode, ...contentNodes) {
     if (writeMode == "SET") {
         nodeToWriteTo.replaceChildren();
         AddNodesToNode(nodeToWriteTo, contentNodes);
-    }else if (writeMode == "APPEND") {
+    } else if (writeMode == "APPEND") {
         AddNodesToNode(nodeToWriteTo, contentNodes);
-    }else{
+    } else {
         throw new NotImplementedException(`${writeMode} is not a supported mode, use the WriteMode class.`);
     }
 };
+
+function DateStringToDate(dateString) {
+    let date = dateString.substring(0, 10);
+    let dateFrags = date.split('-');
+
+    return (`${dateFrags[2]} ${GetMonthByNumber(dateFrags[1])} ${dateFrags[0]}`);
+}
 
 function AddNodesToNode(node, nodesToAdd) {
     nodesToAdd.forEach(nodeToAdd => {
@@ -19,4 +26,51 @@ function AddNodesToNode(node, nodesToAdd) {
     });
 };
 
-export {SetTextContent};
+function GetMonthByNumber(monthNumber) {
+    let monthString;
+
+    switch (monthNumber) {
+        case "01":
+            monthString = "January";
+            break;
+        case "02":
+            monthString = "February";
+            break;
+        case "03":
+            monthString = "March";
+            break;
+        case "04":
+            monthString = "April";
+            break;
+        case "05":
+            monthString = "May";
+            break;
+        case "06":
+            monthString = "June";
+            break;
+        case "07":
+            monthString = "July";
+            break;
+        case "08":
+            monthString = "August";
+            break;
+        case "09":
+            monthString = "September";
+            break;
+        case "10":
+            monthString = "October";
+            break;
+        case "11":
+            monthString = "November";
+            break;
+        case "12":
+            monthString = "December";
+            break;
+        default:
+            monthString = "Unknown Month";
+    }
+
+    return monthString;
+}
+
+export { SetTextContent, DateStringToDate };
