e9160fcb17daeccdc54cbc1ae1e15320223af59d
Adriaan Waem <2ndalgorithm@gmail.com>
2024-09-04 23:17:22 +0200

    removing pipeline :^)

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$file1 = Get-Item -Path .\index.html;
-$file2 = Get-Item -Path .\cat\index.html;
-
-if ($(Get-FileHash $file1 -Algorithm SHA1).Hash -eq $(Get-FileHash $file2 -Algorithm SHA1).Hash){
-    Write-Output "No changes";
-} else{
-    Write-Output "changes";
-}
