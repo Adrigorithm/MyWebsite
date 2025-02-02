9c162fa6e2bc7d17ca8cec12824132a9bf4c8c7e
Adriaan Waem <2ndalgorithm@gmail.com>
2025-02-02 17:53:23 +0100

    tailwindcss v4

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [ pkgs.git pkgs.python314 pkgs.pnpm pkgs.tailwindcss pkgs.vscodium ];
+  buildInputs = [ pkgs.git pkgs.python314 pkgs.pnpm pkgs.vscodium ];
 
   shellHook = ''
     codium --install-extension bradlc.vscode-tailwindcss --force
     codium --install-extension jnoortheen.nix-ide --force
-    pnmp install
+    pnpm install
   '';
 }
