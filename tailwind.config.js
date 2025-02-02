41bf8d46f7e47e986c44bf34f700ae9a0c1e5141
Adriaan Waem <2ndalgorithm@gmail.com>
2025-02-03 00:10:55 +0100

    flowers are pretty

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'black-overlay': 'oklch(0% 0 0 / 50%)',
-        'white-overlay': 'oklch(100% 0 0 / 50%)',
-      }
+      
     },
   },
   plugins: [],
