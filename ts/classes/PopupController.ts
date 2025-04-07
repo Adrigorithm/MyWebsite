4661b01c03ad1c42af3eedb886c157705e3c9427
Adriaan Waem <2ndalgorithm@gmail.com>
2025-04-07 22:39:40 +0200

    the battery is dying :3

--- ts/classes/PopupController.ts
+++ ts/classes/PopupController.ts
@@ -1,13 +1,24 @@
 class PopupController implements IPopupController {
-    popups: HTMLElement[];
+    popups: Map<HTMLElement, boolean>;
 
     constructor() {
-        this.popups = [];
+        this.popups = new Map();
     }
 
-    initialise(popups: Map<HTMLElement[], HTMLElement[] | boolean>): void {
+    initialise(popups: Map<HTMLElement, HTMLElement[] | boolean>): void {
         for (const [popup, trigger] of popups) {
-            
+            this.popups.set(popup, false);
+
+            if (typeof trigger === 'boolean') {
+                if (trigger)
+                    popup.classList.remove("hidden");
+                
+                continue;
+            }
+
+            trigger.forEach(element => {
+                element.addEventListener("click", () => {})
+            });
         }
     }
 
--- ts/interfaces/IPopupController.ts
+++ ts/interfaces/IPopupController.ts
@@ -1,6 +1,6 @@
 interface IPopupController {
-    popups: HTMLElement[];
+    popups: Map<HTMLElement, boolean>;
 
-    initialise(popups: Map<HTMLElement[], HTMLElement[] | boolean>): void;
+    initialise(popups: Map<HTMLElement, HTMLElement[] | boolean>): void;
     work(): void;
 }
